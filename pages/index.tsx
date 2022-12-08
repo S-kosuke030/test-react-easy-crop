f873b36cb7877f20d7c2cf9f140071cb8827ce7c
Kosuke Sato <kosuke.sato030@gmail.com>
2022-12-08 17:05:45 +0900

    kiritori

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,146 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import { Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { useCallback, useEffect, useState } from 'react';
-import Cropper, { Area } from 'react-easy-crop';
-import styles from '../styles/Home.module.css';
+import { Area, MediaSize } from 'react-easy-crop';
+import CropperModal from './CropperModal';
+import getCroppedImg from './getCroppedImg';
+import { saveAs } from 'file-saver';
+
+export const ASPECT_RATIO = 1 / 1;
+export const CROP_WIDTH = 300;
+
+const Container = styled('div')(({ theme }) => ({
+  marginTop: 30,
+  minWidth: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  textAlign: 'center',
+  flexFlow: 'column',
+  '& .file-upload-container': {
+    width: 500,
+    marginTop: 10,
+    '& .button': {
+      backgroundColor: '#00A0FF',
+      color: 'white'
+    }
+  },
+  '& .img-container': {
+    marginTop: 40,
+    width: `${CROP_WIDTH}px`,
+    height: `${CROP_WIDTH / ASPECT_RATIO}px`,
+    display: 'flex',
+    alinItems: 'center',
+    borderRadius: 5,
+    border: '1px solid gray',
+    overflow: 'hidden',
+    backgroundColor: '#EAEAEA',
+    '& .img': {
+      width: '100%',
+      objectFit: 'contain',
+      backgroundColor: '#EAEAEA'
+    },
+    '& .no-img': {
+      backgroundColor: '#EAEAEA',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#000'
+    }
+  }
+}));
 
 export default function Home() {
-  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [isRendered, setIsRendered] = useState(false);
+  /** Cropモーダルの開閉 */
+  const [isOpen, setIsOpen] = useState(false);
+
+  /** アップロードした画像URL */
+  const [imgSrc, setImgSrc] = useState('');
+
+  /** 画像の拡大縮小倍率 */
   const [zoom, setZoom] = useState(1);
-  const [isRendered, setIsRendered] = useState<boolean>(false);
-  const onCropComplete = useCallback((croppedArea: Area, croppedAreaPixels: Area) => {
-    // console.log(croppedArea, croppedAreaPixels);
-  }, []);
+
+  /** 画像拡大縮小の最小値 */
+  const [minZoom, setMinZoom] = useState(1);
+
+  /** 切り取る領域の情報 */
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+
+  /** 切り取る領域の情報 */
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
+
+  /** 切り取ったあとの画像URL */
+  const [croppedImgSrc, setCroppedImgSrc] = useState('');
+
   useEffect(() => {
     setIsRendered(true);
   }, []);
+
+  const onFileChange = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        if (reader.result) {
+          setImgSrc(reader.result.toString() || '');
+          setIsOpen(true);
+        }
+      });
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }, []);
+
+  const onMediaLoaded = useCallback((mediaSize: MediaSize) => {
+    const { width, height } = mediaSize;
+    const mediaAspectRatio = width / height;
+    if (mediaAspectRatio > ASPECT_RATIO) {
+      // 縦幅に合わせてZoomを指定
+      const result = CROP_WIDTH / ASPECT_RATIO / height;
+      setZoom(result);
+      setMinZoom(result);
+    }
+  }, []);
+
+  const onCropComplete = useCallback((croppedArea: Area, croppedAreaPixels: Area) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const showCroppedImage = useCallback(async () => {
+    if (!croppedAreaPixels) return;
+    try {
+      const croppedImage = await getCroppedImg(imgSrc, croppedAreaPixels);
+      console.log('croppedImage', croppedImage);
+      saveAs(croppedImage, 'cropped');
+      // setCroppedImgSrc(croppedImage);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [croppedAreaPixels, imgSrc]);
   return (
     <div className="App">
       {isRendered && (
-        <>
-          <div className="crop-container">
-            <Cropper
-              image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
-              crop={crop}
-              zoom={zoom}
-              aspect={1 / 1}
-              onCropChange={setCrop}
-              onCropComplete={onCropComplete}
-              onZoomChange={setZoom}
-            />
+        <Container>
+          <div className="file-upload-container">
+            <Button variant="contained" component="label" className="button">
+              Upload File
+              <input type="file" hidden onChange={onFileChange} />
+            </Button>
           </div>
-          <div className="controls">
-            <input
-              type="range"
-              value={zoom}
-              min={1}
-              max={3}
-              step={0.1}
-              aria-labelledby="Zoom"
-              onChange={(e) => {
-                setZoom(e.target.valueAsNumber);
-              }}
-              className="zoom-range"
-            />
-          </div>
-        </>
+          <CropperModal
+            crop={crop}
+            setCrop={setCrop}
+            zoom={zoom}
+            setZoom={setZoom}
+            onCropComplete={onCropComplete}
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+            imgSrc={imgSrc}
+            showCroppedImage={showCroppedImage}
+            onMediaLoaded={onMediaLoaded}
+            minZoom={minZoom}
+          />
+        </Container>
       )}
     </div>
   );
